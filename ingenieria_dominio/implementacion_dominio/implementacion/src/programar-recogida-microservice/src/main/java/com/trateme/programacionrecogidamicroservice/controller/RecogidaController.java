93fa960044d11ec55b85314e0dac7a2ad34c701a
Jhmedinaa <36643081+Jhmedinaa@users.noreply.github.com>
2018-09-23 15:39:24 -0500

    se crea white list
    se crea white list

--- ingenieria_dominio/implementacion_dominio/implementacion/src/programar-recogida-microservice/src/main/java/com/trateme/programacionrecogidamicroservice/controller/RecogidaController.java
+++ ingenieria_dominio/implementacion_dominio/implementacion/src/programar-recogida-microservice/src/main/java/com/trateme/programacionrecogidamicroservice/controller/RecogidaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import com.trateme.programacionrecogidamicroservice.models.entity.Recogida;
 import com.trateme.programacionrecogidamicroservice.models.service.IRecogidaService;
 
 
-
+@CrossOrigin(origins= {""})
 @RestController
 @RequestMapping("/api")
 public class RecogidaController {
@@ -24,19 +25,19 @@ public class RecogidaController {
 	@Autowired
 	private IRecogidaService recogidaService;
 	
-	@GetMapping("/ProgramacionServicio/Consultar")
+	@GetMapping("/recogida/all")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Recogida> findAll(){
 		return recogidaService.findAll();
 	}
 	
-	@PostMapping("/ProgramacionServicio/Programar")
+	@PostMapping("/recogida/save")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Recogida save(@RequestBody Recogida r) {		
 		return recogidaService.save(r);
 	}
 	
-	@GetMapping("/ProgramacionServicio/{id}")
+	@GetMapping("/recogida/{id}")
 	public Recogida findById(@PathVariable Long id) {
 		return recogidaService.findById(id);
 	}
