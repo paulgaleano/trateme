9d0b207c16ef68d83a850135adf311f1f5e036f0
Jhmedinaa <36643081+Jhmedinaa@users.noreply.github.com>
2018-09-23 22:33:40 -0500

    Se realizan pruebas
    se hacen correcciones por pruebas

--- ingenieria_dominio/implementacion_dominio/implementacion/src/programar-recogida-microservice/src/main/java/com/trateme/programacionrecogidamicroservice/controller/RecogidaController.java
+++ ingenieria_dominio/implementacion_dominio/implementacion/src/programar-recogida-microservice/src/main/java/com/trateme/programacionrecogidamicroservice/controller/RecogidaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,7 @@ import com.trateme.programacionrecogidamicroservice.models.entity.Recogida;
 import com.trateme.programacionrecogidamicroservice.models.service.IRecogidaService;
 
 
-@CrossOrigin(origins= {""})
+
 @RestController
 @RequestMapping("/api")
 public class RecogidaController {
